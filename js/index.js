9ac6cf7f15311cb17205f5b440d2ebbc04ae9eaa
Shubham Gautam <sg2199203@gmail.com>
2020-12-16 20:57:12 +0530

    Handling CORS and reverted

--- js/index.js
+++ js/index.js
@@ -94,17 +94,17 @@ function setCountry(country) {
 
     //making an call to the API
     var countryVal = 'nil'
-    //this api enables cross-origin requests (since we're having CORS error while working with newsAPI)
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    // //this api enables cross-origin requests (since we're having CORS error while working with newsA)
+    // const proxyUrl = "https://cors-anywhere.herokuapp.com/"
     switch (country) {
         case 'us':
-            countryVal = `${proxyUrl}http://newsapi.org/v2/top-headlines?country=us&apiKey=cb1e0df2ab854320b6090d4947f1cbf2`
+            countryVal = `http://newsapi.org/v2/top-headlines?country=us&apiKey=cb1e0df2ab854320b6090d4947f1cbf2`
             break;
         case 'ind':
-            countryVal = `${proxyUrl}http://newsapi.org/v2/top-headlines?country=in&apiKey=cb1e0df2ab854320b6090d4947f1cbf2`
+            countryVal = `http://newsapi.org/v2/top-headlines?country=in&apiKey=cb1e0df2ab854320b6090d4947f1cbf2`
             break;
         case 'de':
-            countryVal = `${proxyUrl}http://newsapi.org/v2/top-headlines?country=de&apiKey=cb1e0df2ab854320b6090d4947f1cbf2`
+            countryVal = `http://newsapi.org/v2/top-headlines?country=de&apiKey=cb1e0df2ab854320b6090d4947f1cbf2`
             break;
         default:
             break;
