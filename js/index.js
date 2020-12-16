d10495facfc04ad758141a5ea5692253b94152bc
Shubham Gautam <sg2199203@gmail.com>
2020-12-16 12:28:33 +0530

    Some js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,29 @@
 //     var loader = document.querySelector(".loader"); //on class 'loader'
 //     loader.classList.add('display-off-loader');
 // });
+
+// fetch('http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=cb1e0df2ab854320b6090d4947f1cbf2')
+//     .then((response) => response.json())
+//     .then((user) => {
+//         console.log(user.articles);
+//     })
+//     .catch(err => {
+//         console.log('Error:', err);
+//     })
+
+function setLocalStorage(country){
+    //storing country choice in browser
+    window.localStorage.setItem('country', JSON.stringify(country))
+}
+
+function countrySel(e){
+    e.preventDefault();
+    var countryVal = document.querySelector('select').value
+    setLocalStorage(countryVal)
+}
+
+function formSubmitScreen(){
+    var cSlection = document.querySelector('.cont-selection')
+    var form = document.querySelector('form')
+    form.addEventListener('submit', countrySel)
+}
