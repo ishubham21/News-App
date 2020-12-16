b9a350c8f1cca74c219b8e4a7c97ded1dff11672
Shubham Gautam <sg2199203@gmail.com>
2020-12-16 13:00:44 +0530

    Logic completed

--- js/index.js
+++ js/index.js
@@ -3,18 +3,15 @@
 //     loader.classList.add('display-off-loader');
 // });
 
-// fetch('http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=cb1e0df2ab854320b6090d4947f1cbf2')
-//     .then((response) => response.json())
-//     .then((user) => {
-//         console.log(user.articles);
-//     })
-//     .catch(err => {
-//         console.log('Error:', err);
-//     })
+//global variables
+var newsSection = document.querySelector('.news')
+var changeCountry = document.querySelector('.chng-country')
+changeCountry.addEventListener('click', formSubmitScreen)
 
 function setLocalStorage(country){
     //storing country choice in browser
     window.localStorage.setItem('country', JSON.stringify(country))
+    setCountry(country)
 }
 
 function countrySel(e){
@@ -25,6 +22,57 @@ function countrySel(e){
 
 function formSubmitScreen(){
     var cSlection = document.querySelector('.cont-selection')
+    cSlection.classList.add('display-true')
+
     var form = document.querySelector('form')
     form.addEventListener('submit', countrySel)
 }
+
+function printNews(articlesArr) {
+    newsSection.innerHTML = ""
+    for (let i = 0; i < articlesArr.length; i++) {
+        newsSection.innerHTML += `<div>${articlesArr[i].description}</div>`
+    }
+}
+
+function setCountry(country){
+
+    var countryVal = 'nil'
+    switch (country) {
+        case 'us':
+            countryVal = `http://newsapi.org/v2/top-headlines?country=us&apiKey=cb1e0df2ab854320b6090d4947f1cbf2`
+            break;
+        case 'ind': 
+            countryVal = `http://newsapi.org/v2/top-headlines?country=in&apiKey=cb1e0df2ab854320b6090d4947f1cbf2`
+            break;
+        case 'de':
+            countryVal = `http://newsapi.org/v2/top-headlines?country=de&apiKey=cb1e0df2ab854320b6090d4947f1cbf2`
+            break;
+        default:
+            break;
+    }
+
+    fetch(countryVal)
+    .then((response) => response.json())
+    .then((user) => {
+        printNews(user.articles)
+        console.log(user.articles);
+    })
+    .catch(err => {
+        console.log('Error:', err);
+    })
+}
+
+function checkCountry(){
+    if (JSON.parse(window.localStorage.getItem('country'))) {
+        var country = JSON.parse(window.localStorage.getItem('country'))
+        console.log(country);
+        setCountry(country)
+    } 
+    else{
+        formSubmitScreen()
+    }
+}
+
+checkCountry()
+
